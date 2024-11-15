31b080eb67ebd28a51980f1942cb8d4a7478c76d
semirayurushashank <161278982+semirayurushashank@users.noreply.github.com>
2024-11-15 14:58:53 +0530

    Add files via upload

---
+++ Lab Manual/IP Lab/Exercise/webpage/sample.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const s = num.map((ele) => {
+    return Math.pow(ele, 3);
+});
+
+console.log(s);
+
+// function square(ele) {
+//     return Math.pow(ele, 2);
+// }
